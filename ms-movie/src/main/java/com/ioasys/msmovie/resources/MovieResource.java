e6c7d00f44a721b98ec0c756c089ec1148df902a
Renan Cidrao <renan.cidrao@sejus.ce.gov.br>
2021-07-30 11:13:14 -0300

    salvando novos filmes

--- ms-movie/src/main/java/com/ioasys/msmovie/resources/MovieResource.java
+++ ms-movie/src/main/java/com/ioasys/msmovie/resources/MovieResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +34,12 @@ public class MovieResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Movie> findById(@PathVariable Long id) {
 		Movie obj = repository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Movie> save(@RequestBody Movie movie) {
+		Movie obj = repository.save(movie);
 		return ResponseEntity.ok(obj);
 	}
 }
